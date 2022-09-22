afd8ef0b914db60a80bfc21cd06a7372e448c4ea
Walter Oswaldo <oswaldo@MacBook-Pro-de-Walter.local>
2022-09-21 22:21:24 -0600

    feat(arbol B):
    Se agregra un ejemplo de arbol B programado en C++

--- arboles/main.cpp
+++ arboles/main.cpp
@@ -1,11 +1,30 @@
-#include "binarioBusqueda.cpp"
 
+#include "B.cpp"
 int main()
 {
-    BST<int> *enteros = new BST<int>();
-    enteros->insertar(8);
-    enteros->insertar(3);
-    enteros->insertar(10);
 
-    enteros->inOrden();
+    /**************** PRUEBA ARBOL B ****************/
+    B arbolB;
+    arbolB.insertar(75);
+    arbolB.insertar(40);
+    arbolB.insertar(23);
+    arbolB.insertar(8);
+    arbolB.insertar(6);
+    arbolB.insertar(37);
+    arbolB.insertar(32);
+    arbolB.insertar(45);
+    arbolB.insertar(25);
+    arbolB.insertar(1);
+    arbolB.insertar(88);
+    arbolB.insertar(29);
+    arbolB.insertar(4);
+    arbolB.insertar(11);
+    arbolB.insertar(39);
+    arbolB.insertar(14);
+    arbolB.insertar(66);
+    arbolB.insertar(57);
+    arbolB.insertar(90);
+    arbolB.insertar(24);
+    arbolB.insertar(12);
+    arbolB.insertar(7);
 }
